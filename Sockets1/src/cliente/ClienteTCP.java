c67a2d4939b52fc02493af8f60d603c4e4c6daf4
Usuario <Usuario@DESKTOP-6B1PL7N.home>
2023-03-07 09:46:47 +0100

    otra vez

--- Sockets1/src/cliente/ClienteTCP.java
+++ Sockets1/src/cliente/ClienteTCP.java
@@ -1,42 +1,50 @@
 package cliente;
 
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class ClienteTCP {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-		// Se crea una nueva conexión con el servidor especificando la IP y el puerto
-	    Socket servidor = new Socket("127.0.0.1", 7000);
-	    System.out.println("Hola desde el servidor");
-
-	    // Se establecen las entradas y salidas para la comunicación con el servidor
-	    PrintWriter pw = new PrintWriter(servidor.getOutputStream(), true);
-
-	    // Se utiliza un scanner para recibir los mensajes del usuario
-	    Scanner sc = new Scanner(System.in);
-	    String enviarMensaje;
-
-	    while (true) {
-
-	        System.out.println("Escribe el mensaje a enviar");
-	        enviarMensaje = sc.nextLine();
-	        // Se envía el mensaje al servidor
-	        pw.println(enviarMensaje);
-	        System.out.println("Mensaje enviado");
-
-	        if (enviarMensaje.equals("fin")) {
-	            break;
-	        }
-	    }
-	    System.out.println("Comunicación finalizada");
-	    sc.close();
-	    servidor.close();
-
+		BufferedReader inTeclado = new BufferedReader(new InputStreamReader(System.in));
+		String mensajeTeclado = "";
+		String mensajeServidor = "";
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			
+			Socket socket = new Socket("127.0.0.1", 7000);
+			
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+			mensajeServidor = in.readUTF();
+			System.out.println(mensajeServidor);
+			
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			
+			do {
+				
+				System.out.println("\nEscribe el mensaje a enviar");
+				mensajeTeclado = sc.nextLine();
+				out.writeUTF(mensajeTeclado);
+				
+				System.out.println("Mensaje enviado");
+				mensajeServidor = in.readUTF();
+				System.out.println("Mensaje recibido del servidor: " + mensajeServidor);
+				
+			} while (!mensajeTeclado.equals("fin"));
+			
+			socket.close();
+			System.out.println("\nComunicacion finalizada");
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.exit(1);;
+		}
 	}
-	}
+
+}
--- Sockets1/src/servidor/ServidorTCP.java
+++ Sockets1/src/servidor/ServidorTCP.java
@@ -1,45 +1,51 @@
 package servidor;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServidorTCP {
 
-	public static void main(String[] args) throws IOException {
-		
-		 // Se crea un nuevo objeto ServerSocket y se especifica el puerto a utilizar
-	    ServerSocket servidor = new ServerSocket(7000);
-	    System.out.println("Esperando a que alguien conecte");
-	    
-	    // El servidor espera a que un cliente se conecte y se acepta la conexión
-	    Socket cliente = servidor.accept();
-	    System.out.println("Ha conectado " + cliente.getInetAddress().getHostAddress() + ":" + cliente.getPort());
-	    
-	 // Se establecen las entradas y salidas para la comunicación con el cliente
-	    BufferedReader br = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-	    PrintWriter pw = new PrintWriter(cliente.getOutputStream(), true);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-	    // Se espera a recibir un mensaje del cliente y se responde con el mismo mensaje en mayúsculas
-	    String enviarMensaje;
-	    while ((enviarMensaje = br.readLine()) != null) {
-	        System.out.println("Mensaje recibido: " + enviarMensaje);
-	        pw.println(enviarMensaje.toUpperCase());
-	        System.out.println("Se ha contestado el mismo mensaje en mayúsculas: " + enviarMensaje.toUpperCase());
-	        if (enviarMensaje.equals("fin")) {
-	            break;
-	        }
-	    }
-
-	    System.out.println("Comunicación finalizada");
-
-	    cliente.close();
-	    servidor.close();
-	    
+		try {
+			
+			ServerSocket socket = new ServerSocket(7000);
+			System.out.println("Esperando a que alguien conecte");
+			Socket socket_Cli = socket.accept();
+			
+			String cliente = socket_Cli.getRemoteSocketAddress().toString();
+			System.out.println("Ha conectado: " + cliente);
+			
+			DataInputStream in = new DataInputStream(socket_Cli.getInputStream());
+			DataOutputStream out = new DataOutputStream(socket_Cli.getOutputStream());
+			
+			out.writeUTF("Hola desde el servidor");
+			
+			String mensaje = "";
+			String mensaje2 = "";
+			
+			do {
+				
+				System.out.println("\nEsperando recibir mensaje");
+				mensaje = in.readUTF();
+				System.out.println("Mensaje recibido: " + mensaje);
+				mensaje2 = mensaje.toUpperCase();
+				out.writeUTF(mensaje2);
+				System.out.println("Se ha contestado el mismo mensaje en mayusculas");
+				
+			} while (!mensaje.equals("fin"));
+			
+			socket_Cli.close();
+			System.out.println("\nComunicacion finalizada");
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.exit(1);
+			
+		}
 	}
+
 }
-
-
